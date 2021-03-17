bbb942623e5be7528ab3b915756509430b640be6
louis <louis.mackenziesmith@googlemail.com>
2021-03-17 12:13:19 +0100

    e

--- torch_ac/algos/a2c.py
+++ torch_ac/algos/a2c.py
@@ -76,13 +76,17 @@ class A2CAlgo(BaseAlgo):
 
         self.optimizer.zero_grad()
         update_loss.backward()
-        update_grad_norm = sum(p.grad.data.norm(2) ** 2 for p in self.acmodel.parameters()) ** 0.5
+        # update_grad_norm = sum(p.grad.data.norm(2) ** 2 for p in self.acmodel.parameters()) ** 0.5
+
+        # making this change as grad_norm is a tensor, which shows up as a string later in logs
+        update_grad_norm = sum(p.grad.data.norm(2).item() ** 2 for p in self.acmodel.parameters()) ** 0.5
         torch.nn.utils.clip_grad_norm_(self.acmodel.parameters(), self.max_grad_norm)
         self.optimizer.step()
 
         # Log some values
 
         logs = {
+            "X_entropy": update_entropy,
             "entropy": update_entropy,
             "value": update_value,
             "policy_loss": update_policy_loss,
